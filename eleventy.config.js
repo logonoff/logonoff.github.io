b06f418074c2c032238beb33cd07e06d4ec71fb6
logonoff <git@logonoff.co>
2025-09-28 23:00:29 -0400

    why do we need 11ty.ts at all

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,12 +1,15 @@
-export default function (eleventyConfig) {
+const { defineConfig } = require('11ty.ts'); // i'm shocked we need this T_T
+
+export default defineConfig(config => {
 	// Output directory: _site
 
+	/** Passthrough Copy **/
 	// Copy `assets/` to `_site/assets`
-	eleventyConfig.addPassthroughCopy("assets");
+	config.addPassthroughCopy("assets");
 
-  // Copy `profile/` to `_site/profile`
-	eleventyConfig.addPassthroughCopy("profile");
+	// Copy `profile/` to `_site/profile`
+	config.addPassthroughCopy("profile");
 
-  // Copy `projects/` to `_site/projects`
-	eleventyConfig.addPassthroughCopy("projects");
-};
+	// Copy `projects/` to `_site/projects`
+	config.addPassthroughCopy("projects");
+});
