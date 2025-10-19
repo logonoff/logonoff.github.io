cc52778c701dae55dd3e36cea070e3e7543c8cf8
logonoff <git@logonoff.co>
2025-10-18 21:39:22 -0400

    minify html

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from '11ty.ts'; // i'm shocked we need this T_T
+import htmlmin from "html-minifier";
 
 export default defineConfig(config => {
 	// Output directory: _site
+
+	/** Minify HTML */
+	config.addTransform("minify", (content, outputPath) => {
+		switch (outputPath ? outputPath.split('.').pop() : null) {
+			case "html":
+				return htmlmin.minify(content, {
+					removeComments: true,
+					collapseWhitespace: true,
+				});
+			default:
+				return content;
+		}
+	});
 
 	/** Passthrough Copy **/
 	// Copy `assets/` to `_site/assets`
